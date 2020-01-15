857ba822f7bf19ba86b9d1405fcea997c91663f3
lee-wood <leemaewood@gmail.com>
2020-01-15 00:52:52 -0500

    simple server base built, server working

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+
 const port = 5000;
+
 const server = express();
 
+server.listen(5000, () => {
+  console.log('server listening on port 5000!')
+});
+
+//*GLOBAL MIDDLEWARE:
+
 server.use(express.json());
+
+//*MIDDLEWARE:
+
+
+
 
