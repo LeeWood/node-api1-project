356a24d0ce511a6a763416476ce6c6495aa63163
lee-wood <leemaewood@gmail.com>
2020-01-15 01:16:35 -0500

    endpoint to get list of all users in database

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
+//*VARIABLES
+
 const express = require('express');
-
 const port = 5000;
-
 const server = express();
+const db = require('./data/db');
 
-server.listen(5000, () => {
-  console.log('server listening on port 5000!')
+//*DIRECT SERVER 
+
+server.listen(port, () => {
+  console.log(`server listening on port:${port}!`)
 });
 
 //*GLOBAL MIDDLEWARE:
 
 server.use(express.json());
 
-//*MIDDLEWARE:
+//*ENDPOINTS:
 
+//base
+server.get('/', (req, res) => {
+  res.send('Hello from the server :)')
+});
 
+//GET(all)
 
+server.get('/users', (req, res) => {
+  db.find()
+    .then(users => {
+      if(users) {
+        res.status(200).json(users);
+      } else {
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+      };
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err })
+    });
+});
 
+//GET(specific)
+
+//POST
+
+//PUT
+
+//DELETE
